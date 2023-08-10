598542f52645325a679cf4ab69fd0ff447f26245
Baavro <81712871+Baavro@users.noreply.github.com>
2023-08-10 18:27:19 +0530

    Update Axis_prompts.py

--- api/Axis_prompts.py
+++ api/Axis_prompts.py
@@ -1,6 +1,7 @@
 CV = """Education:\nBachelor of Science in Computer Science\nXYZ University, Anytown, USA\nExpected Graduation: May 2024\nRelevant Courses:\n- Data Structures and Algorithms\n- Object-Oriented Programming\n- Database Management Systems\n- Web Development\n- Software Engineering\n- Artificial Intelligence\nInternship:\nSoftware Engineering Intern, XYZ Tech Solutions, Anytown, USA\nJune 2023 - August 2023\n- Collaborated with a team of developers on the design and implementation of a web application for customer data management.\n- Assisted in writing efficient code and conducting unit tests, resulting in a 20% reduction in application loading time.\n- Gained valuable experience in Agile development methodologies and version control systems.\nProjects:\n1. TaskMaster - Mobile Task Management App\n   - Developed a mobile application for task management using Flutter framework.\n   - Implemented features such as user authentication, task categorization, and real-time updates.\n   - Published the app on Google Play Store, receiving positive user feedback and over 1000 downloads.\n2. Shopify - E-commerce Web Platform\n   - Created a web-based e-commerce platform using HTML, CSS, and JavaScript.\n   - Integrated payment gateways and ensured secure transactions.\n   - Conducted load testing and optimized website performance.\nCompetitions:\n- AI Chatbot Challenge, XYZ Hackathon, Anytown, USA, March 2023\n  - Led a team of four students in developing an AI-powered chatbot for customer support.\n  - Utilized natural language processing techniques to enhance chatbot interactions.\n  - Received 2nd place in the competition.\n- Health & Fitness App Challenge, ABC Tech Fest, Anytown, USA, November 2022\n  - Participated in a hackathon and designed a health and fitness mobile app.\n  - Implemented a unique feature that allowed users to track and analyze their fitness progress.\n  - Earned recognition for the app's innovative design and functionality.\nSkills:\n- Programming Languages: Java, Python, JavaScript\n- Web Development: HTML, CSS, JavaScript, React\n- Mobile App Development: Flutter\n- Database Management: SQL, MongoDB\n- Version Control: Git\n- Problem-Solving: Analytical thinking, creative problem-solving\n- Teamwork: Collaboration, communication, and adaptability\nLanguages:\n- English (Fluent)\n- Spanish (Conversational)\nReferences:\nAvailable upon request.\n"""
 
-fields = [
+def fields():
+    field_lst = [
 "ACADEMIC QUALIFICATION",
 "JOB EXPERIENCE",
 "PROJECTS",
@@ -11,6 +12,7 @@ fields = [
 "SKILLS AND EXPERTISE",
 "EXTRACIRRICULAR ACTIVITIES"
 ]
+    return field_lst
 
 
 CV_info = {'ACADEMIC QUALIFICATION': 'Bachelor of Science in Computer Science\nXYZ University, Anytown, USA\nExpected Graduation: May 2024',
@@ -119,3 +121,71 @@ def CV_converter_template(CV,field):
     """
     return prompt
 
+
+def jd_description_template(jd_contents):
+    template = f"""You are a human resource manager. You are reading a job description for a job opening at your company. Given the job description, you need to answer the following questions:
+        {jd_contents}
+        Human: Which of the following sections are present in the job description? ['Job Title', 'Job Summary', 'Responsibilities', 'Requirements', 'Preferred Qualification', 'Company Overview', 'Benefits/Perks', 'Salary Range', 'Location', 'Contact Information']. Please just list the sections. Remember to put a comma between each section.
+        AI:"""
+    return template
+
+def QuestionsPrompt(category,list_topic,history):
+        temp=f""" 
+    You are an Interviewer whose job is to determine my level of understanding/depth of a candidate. You will be given a list of topics below which are extracted from the {category}. Your Job is to ask Questions Related to The list of topics. Below are the set of instructions you need to follow:
+    [INSTRUCTIONS]
+        -Start by Introducing yourself as Robin and displaying the first Question.Just Introduce once at the starting of Interview. Don't Introduce after every topic.
+        -You will not repeat the question. Keep track of history to check which questions you have asked before. If the candidate gives and incomplete answer don't prompt the same question back. Move to the next question.
+        -You will not repeat the topic you asked before. if the evaluation of the topic is done don;t ask question on that topic.
+        -End the Quiz after 10 Questions. keep track using the HISTORY.
+        -[IMPORTANT] You will NEVER display or return the content present in  <context> or <HISTORY> at any point of time.
+        -IF the user return "QUIZ ENDS". Then end the quiz and evaluate and After Evaluating you will return the answer in a json format. Eg- ["Topic":<List of Topic>,"Depth":<depth of student>,"Weak Topic": <List of topic which need improvement>]
+        -You have to rate on a scale of 1 to 10. 1 being the complete novice and 10 being an expert.
+        -Make sure you cover all the topic in the list of topic thoroughly by asking questions on all the topics in the list. If the topic is related to coding ask more coding questions. If the topic is related to maths then focus on maths.
+        -You have to ask maximum of 3 question per topic and  maximum 10 questions for all the topic combined and wait for the student's response.
+        -Ask a question then wait for response then ask another question. Ask a single question at a time.
+        -YOU JOB IS JUST TO ASK QUESTIONS.
+        -[IF] User response is correct ask a <harder questions or higher depth question> [ELSE] Ask a similar or an <easier question or less depth question>
+        -Make sure that you go in depth of every topic. A student should have in depth knowledge to pass your quiz.
+        -Ask Subjective Questions and [IF] the topic/keyword is related to Technology then ask coding questions as well.
+        -If the user prompt something which is not related to question then display the question again.
+        -You have to judge that the candidate knows the topic in depth or not. Or he just knows the topic superficially.
+    [INSTRUCTIONS ABOUT EVALUATING]
+        -Context have the information about the just previous question. If context is none that means it is the first question
+        -HISTORY contains the information about the all previously asked question. If the coding or the theoretical answer is not perfect then give less points.  
+        -Give more weightage to the coding and the theoretical problems. You have to return a combined score of all the topic
+        -You are a strict tutor who asks hard questions and give less marks. Your aim is to evaluate the candidate and give them the score on the basis of understanding of the topic as a whole with complete honesty.
+        -After Evaluating you will return the answer in a json format. Eg- ["Topic": {category} "Evaluation","Depth":<depth of student>,"Weak Topic": <List of topic which need improvement>]
+
+        List_Topic:{list_topic}
+        Below is the History of all the questions aksed by the AI. !!! Don't Ever Display it !!!. Use it only to keep track of number of questions and Use it to calculate the depth of student. Nothing else. Don't Display it.
+        HISTORY:{history}
+
+        From Here The Interview Starts :
+        """
+        return temp
+
+def summary_template(history,question,query,result):
+    template = f"""Here are the instructions to make question memory, follow them to make question memory that can be used for later to extract information about the question and it's outcome adding onto the previous summary returning a new summary.
+    -Summary will be in format of "["Question":<Question asked by AI>,"Result":<"Score on the basis of 1-10">]\n"
+    - <Question> contains the question asked by the AI and the type. Extract the type as well from <Question>.
+    - <Human_response> shows the response from the human.
+    - <Result> contanins the outcome of the question.
+    Current summary:
+    {history}
+    New lines of conversation:
+    Question:{question}
+    Human_response: {query}
+    Result: {result}
+    
+    New summary:"""
+    print(template)
+    return template
+
+def keyword_prompt(catergory):
+        PROMPT=f"""You are an Interviewer. Your job is to find the key word on the basis of {catergory}. Make Key words on the basis of INSTRUCTONS below:
+                    [INSTRUCTIONS]
+                    -Keywords/Topic will be used to further ask question and evaluate further evaluate the candidate.
+                    -Extract the topic which are essential from the {catergory}. Give the 5 most important topics. 
+                    -Returns Your Answer in form of a python list. Eg-[keyword1,keyword2,keyword3]
+                    """ 
+        return PROMPT
